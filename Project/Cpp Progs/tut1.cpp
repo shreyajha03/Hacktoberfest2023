c2077903242317dd04002b188414bd39cb393c5a
Shreya Jha <98100329+shreyajha03@users.noreply.github.com>
2023-10-03 20:03:05 +0530

    tut1.cpp added

---
+++ Project/Cpp Progs/tut1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int a = 4, b = 5;
+    float pi = 3.14;
+    char u = 'u';
+    cout<<"This is tutorial 4. \nHere the value of a is "<<a<<". \nThe value of b is "<<b;
+    cout<<"\nThe value of pi is "<<pi;
+    cout<<"\nThe value of character is "<<u;
+    return 0;
+}
